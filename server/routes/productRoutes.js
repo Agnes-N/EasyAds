542aec9bb6a1576758a2f4b4a7d0e915191ea0c7
Agnes <aggyreina@gmail.com>
2020-09-07 10:57:29 +0200

    user can view single product

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,9 +1,11 @@
 import Router from 'express';
 import TokenHandler from '../middlewares/tokenHandler';
 import ProductController from '../controllers/productController';
+import Validations from '../middlewares/validations';
 
 const router = Router();
 
 router.get('/products', TokenHandler.verifyToken, ProductController.retrieveAllProducts);
+router.get('/products/:productId', TokenHandler.verifyToken, Validations.validateProductId, ProductController.retrieveOneProduct);
 
 export default router;
